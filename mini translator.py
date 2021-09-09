fdf7b91138aaa785ad42d2ecab6dbeca02475abf
Alyssa <58712807+abenj4404@users.noreply.github.com>
2021-09-08 21:35:55 -0400

    Added .py file

---
+++ mini translator.py
@@ -0,0 +1,13 @@
+#building a translator
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "aeiouAEIOU":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
+
